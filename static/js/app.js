0151301e4a073e24fd64765e578fd234dc7b67f0
lukinoo <luka.bliadze.2015@mail.ru>
2023-02-28 03:56:38 +0400

    NEW: adding scrollTop method to scroll top when nav is active

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,7 @@ const navBurger = document.getElementById("nav-burger");
 const htmlElement = document.querySelector("html");
 
 function navBarActiveHandler() {
+  document.documentElement.scrollTop = 0;
   htmlElement.classList.toggle("overflow-h");
   navBurger.classList.toggle("nav-burger__active");
   navLinks.classList.toggle("nav-links__active");
